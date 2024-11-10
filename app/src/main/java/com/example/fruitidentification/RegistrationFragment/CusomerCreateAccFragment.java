04762ffd34ea2d021fcd8c5fd16b262eab33094e
Richard <Richard@email>
2024-11-10 14:19:46 +0800

    AddedFragFolder

--- app/src/main/java/com/example/fruitidentification/RegistrationFragment/CusomerCreateAccFragment.java
+++ app/src/main/java/com/example/fruitidentification/RegistrationFragment/CusomerCreateAccFragment.java
@@ -1,4 +1,4 @@
-package com.example.fruitidentification;
+package com.example.fruitidentification.RegistrationFragment;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.fruitidentification.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CustomerCreateAccFragment#newInstance} factory method to
+ * Use the {@link CusomerCreateAccFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CustomerCreateAccFragment extends Fragment {
+public class CusomerCreateAccFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class CustomerCreateAccFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CustomerCreateAccFragment() {
+    public CusomerCreateAccFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class CustomerCreateAccFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CustomerCreateAccFragment.
+     * @return A new instance of fragment CusomerCreateAccFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CustomerCreateAccFragment newInstance(String param1, String param2) {
-        CustomerCreateAccFragment fragment = new CustomerCreateAccFragment();
+    public static CusomerCreateAccFragment newInstance(String param1, String param2) {
+        CusomerCreateAccFragment fragment = new CusomerCreateAccFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class CustomerCreateAccFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer_create_acc, container, false);
+        return inflater.inflate(R.layout.fragment_cusomer_create_acc, container, false);
     }
 }
