e45f7c47e2ae2a0437d793b36a59287a382bd66c
Richard <Richard@email>
2024-11-10 14:46:24 +0800

    RemoveFragFolder

---
+++ app/src/main/java/com/example/fruitidentification/CustomerCreateAcc.java
@@ -0,0 +1,14 @@
+package com.example.fruitidentification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerCreateAcc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_create_acc);
+    }
+}
