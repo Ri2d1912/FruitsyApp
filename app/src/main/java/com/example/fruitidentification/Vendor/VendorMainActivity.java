258669c1a1362450bbf40e1372dbfc0e6398945d
Jhaztine <jhaztinemikha@gmail.com>
2024-11-13 23:18:54 +0800

    vendor profile

--- app/src/main/java/com/example/fruitidentification/Vendor/VendorMainActivity.java
+++ app/src/main/java/com/example/fruitidentification/Vendor/VendorMainActivity.java
@@ -2,25 +2,49 @@ package com.example.fruitidentification.Vendor;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import com.example.fruitidentification.R;
+import com.example.fruitidentification.databinding.ActivityMainBinding;
 
 public class VendorMainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_vendor_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new Fragment_vendor_profile());
+
+        binding.bottomNav.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.menu_profile:
+                    replaceFragment(new Fragment_vendor_profile());
+                    break;
+                case R.id.menu_products:
+                    break;
+                case R.id.menu_orders:
+                    break;
+                case R.id.menu_identifier:
+                    break;
+                case R.id.menu_more:
+                    break;
+            }
+
+            return true;
         });
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
